090d046ea14900d790a3faee9152e6dfe850b702
davidsiregar <39730321+davidsiregar@users.noreply.github.com>
2021-11-01 18:19:50 +0700

    Create 1.php
    menggunakan pemprograman php

---
+++ 1.php
@@ -0,0 +1,13 @@
+  function generateRandom($lenght=28)
+    {
+        $character= "0123456789abcdefghijklmopqrsutvwxyz";
+        $characterLeng= strlen($character);
+        $randomString="";
+        for( $i=0;$i<=$lenght;$i++)
+        {
+            $randomString= $character[rand(0,$characterLeng-1)];
+            echo $randomString;
+        }
+    }
+    
+    generateRandom();
